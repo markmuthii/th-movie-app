f6001afde830d5a6292a4cff0a605df667677245
Mark Muthii <mark.muthii@strathmore.edu>
2025-07-02 11:56:28 +0300

    Fetch single movie

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,10 +1,38 @@
-import { Link } from "react-router";
+import { Link, useParams } from "react-router";
 import { Hero } from "../components/Hero";
+import { useEffect, useState } from "react";
+import { getMovies } from "../lib/api";
 
 const Movie = () => {
+  const { movieID } = useParams();
+  const [movie, setMovie] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const { movies } = await getMovies();
+
+        setMovie(movies.find((movie) => movie.id == movieID));
+
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchMovie();
+  }, []);
+
+  // const movie = moviesArray.find((movie) => movie.id == movieID);
+
   return (
     <>
-      <Hero />
+      {loading ? (
+        "Loading..."
+      ) : (
+        <Hero title={movie.title} overview={movie.overview} />
+      )}
 
       <Link to="/" className="bg-green-600 text-white px-6 py-1 cursor-pointer">
         Back to Home
