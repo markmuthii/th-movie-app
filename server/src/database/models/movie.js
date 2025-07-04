d89c78349d94dc6e4b9ead7f0229ce1dc830a0c6
Mark Muthii <mark.muthii@strathmore.edu>
2025-07-04 13:37:25 +0300

    Add comments in model

--- server/src/database/models/movie.js
+++ server/src/database/models/movie.js
@@ -1,24 +1,32 @@
+// This file defines the Movie model schema for MongoDB using Mongoose
 import { Schema, model } from "mongoose";
 
+// Define the schema for a Movie document
 const movieSchema = new Schema({
+  // URL or path to the movie's image/poster
   image: {
     type: String,
     required: true,
   },
+  // Title of the movie
   title: {
     type: String,
     required: true,
   },
+  // Release year of the movie
   year: {
     type: String,
     required: true,
   },
+  // Short description or overview of the movie
   overview: {
     type: String,
     required: true,
   },
 });
 
+// Create the Movie model from the schema
 const Movie = model("Movie", movieSchema);
 
+// Export the Movie model to be used elsewhere in the application
 export { Movie };
